73aafcadbf9727ef00180ecb3374398975586eca
*ItIEXN4rYT <shou.ueda@kuno-corp.com>
2022-12-14 17:08:29 +0900

    完成

--- sample.js
+++ sample.js
@@ -7,12 +7,23 @@ window.onload = function () {
     // URLSearchParamsオブジェクトを取得
     let params = url.searchParams;
 
-    // getメソッド
-    //　param受け取り
-    let land = params.get("land")
-    /*タグを挿入するオブジェクトを取得*/
-    const firstViewElement = document.getElementById('hoge');
-    firstViewElement.insertAdjacentHTML('beforebegin', `${land}`);
-    
+    //　param受け取り　.get(パラメータ名)
+    let GetImg_url = params.get("img_url")
+    let GetlandName = params.get("land_name") 
+    let GetlandOutline = params.get("land_outline") 
+    let Getbusinessday = params.get("businessday") 
+    let Getaddress = params.get("address")
+    let Getclosedday = params.get("closed_day")  
+    let Getofficiallink = params.get("officiallink") 
+
+    //パラメータ渡し
+    const img_url = document.getElementById('img_url').innerHTML = `<img src=${GetImg_url}>`;
+    const land_name = document.getElementById('land_name').innerHTML = GetlandName;
+    const land_outline = document.getElementById('land_outline').innerHTML = GetlandOutline;
+    const businessday = document.getElementById('businessday').innerHTML = `営業時間:${Getbusinessday}`;
+    const address = document.getElementById('address').innerHTML = `住所:${Getaddress}`;
+    const closed_day = document.getElementById('closed_day').innerHTML = `休館日:${Getclosedday}`;
+    const officiallink = document.getElementById('official_link').innerHTML = `<a href=${Getofficiallink}>${Getofficiallink }`;
+
 
 };
